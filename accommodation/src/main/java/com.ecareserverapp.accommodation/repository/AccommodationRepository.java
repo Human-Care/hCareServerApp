e3d49c22a48889b66cc95d05a250646b2c13d987
eseev-german <eseev.german@yandex.ru>
2017-11-26 12:11:56 +0300

    Revert "Feature/accommodation service"

--- accommodation/src/main/java/com.ecareserverapp.accommodation/repository/AccommodationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecareserverapp.accommodation.repository;
-
-import com.ecareserverapp.accommodation.entity.Accommodation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
-}
