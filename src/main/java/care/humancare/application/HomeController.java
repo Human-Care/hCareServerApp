2c7a7247ebf624af6ec7f733784a9103dd91799f
TishkevichLeonid <leonidtishkevich@gmail.com>
2018-04-15 21:24:41 +0300

    it woooorks

--- src/main/java/care/humancare/application/HomeController.java
+++ src/main/java/care/humancare/application/HomeController.java
@@ -1,8 +1,10 @@
 package care.humancare.application;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
 
@@ -11,6 +13,7 @@ public class HomeController {
 //        Map<String, String> model = new HashMap<>();
 //        model.put("name", "a");
 //        return new ModelAndView("index", (Map<String, ?>) null);
+        System.out.println("******** index is started *************");
         return "index";
     }
 }
